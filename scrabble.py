93af67cb95cedee34a5d8a7cbdca5796bf6ecf2d
Kai-Bailey <kai.bailey@icloud.com>
2018-03-30 15:17:06 -0600

    Started building board and cell class Boardinit function does not work

--- scrabble.py
+++ scrabble.py
@@ -1,11 +1,42 @@
 import pygame
 from pygame.locals import *
 
+
 class Board:
-    pass
+    def __init__(self):
+        # Array of board each inner list is a row
+        self.board = []
+        # List of players playing the game
+        self.players = []
+        # Index in list players of whos turn it is
+        self.turn = 0
+        
+    def initBoard(self):
+        triple_word = set([(0,0), (0,7), (0,14), (7,0), (7,14), (14,0), (14,7), (14,14) ])
+        
+        for i in range(15):
+            self.board.append([])
+            for j in range(15):
+                self.board[i].append
+                if (i,j) in triple_word:
+                    self.board[i].append(Cell(None, 1, 3))
+                print(self.board[i][j].word_mul, end=" ")
+
 
 class Cell:
-    pass
+    def __init__(self, letter, letter_mul, word_mul):
+        self.letter = letter
+        self.word_mul = word_mul
+        self.letter_mul = letter_mul
+
+    def score(self):
+        return letter_scores[self.letter]*self.letter_mul
+
+    def setLetter(self, letter):
+        self.letter = letter
+
+    def getLetter(self):
+        return self.letter
 
 class Player:
     pass
@@ -45,6 +76,8 @@ def main():
     for i in range(15):
         for j in range(15):
             pygame.draw.rect(background, (70, 96, 91), (0+30*i, 0+30*j, 30, 30), 3)
+            
+
 
     running = True
     while running:
@@ -59,4 +92,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+
+    board = Board()
+    board.initBoard()
