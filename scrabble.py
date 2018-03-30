cb0c8211cb737eb59a4488d882bda1e89c0c8143
CodyGramlich <32050152+CodyGramlich@users.noreply.github.com>
2018-03-30 17:36:46 -0600

    added letter score dictionary

--- scrabble.py
+++ scrabble.py
@@ -1,6 +1,11 @@
 import pygame
 from pygame.locals import *
 
+letter_scores = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2,
+                'H': 4, 'I': 1, 'J':8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1,
+                'P': 3, 'Q': 10, 'R': 1, 'S': 1 'T': 1, 'U':1, 'V': 4, 'W': 4,
+                 'X': 8, 'Y': 4, 'Z': 10}
+
 class Board:
     pass
 
@@ -37,10 +42,10 @@ def main():
     # background.blit(tile, (0, 0))
 
 
-    # font = pygame.font.Font(None, 13)
-    # letter = font.render('A', 1, (10, 10, 10))
-    # letterpos = letter.get_rect(centerx=tile.get_width()/2)
-    # tile.blit(letter, (0,0))
+    font = pygame.font.Font(None, 13)
+    letter = font.render('A', 1, (10, 10, 10))
+    letterpos = letter.get_rect(centerx=tile.get_width()/2)
+    tile.blit(letter, (0,0))
 
     for i in range(15):
         for j in range(15):
