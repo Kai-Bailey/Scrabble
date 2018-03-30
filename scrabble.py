1b73126ed4547bd42b23a8e4e6c6cd87eaf3ab5f
Kai-Bailey <kai.bailey@icloud.com>
2018-03-30 17:06:38 -0600

    Complete board init function

--- scrabble.py
+++ scrabble.py
@@ -13,15 +13,24 @@ class Board:
         
     def initBoard(self):
         triple_word = set([(0,0), (0,7), (0,14), (7,0), (7,14), (14,0), (14,7), (14,14) ])
-        
+        double_word = set([(1,1), (2,2), (3,3), (4,4), (1,13), (2,12), (3,11), (4,10), (10,4), (11,3), (12,2), (13,1), (10,10), (11,11), (12,12), (13,13)])
+        triple_letter = set([(1,5), (1,9), (5,1), (5,5), (5,9), (5,13), (5,1), (9,1), (9,5), (9,9), (9,13), (13,5), (13,9) ])
+        double_letter = set([(0,3), (0,11), (2,6), (2,8), (3,0), (3,7), (3,14), (6,2), (6,6), (6,8), (6,12), (7,3), (7,11), (8,2), (8,6), (8,8), (8,12), (14,3), (14,11), (12,6), (12,8), (11,0), (11,7), (11,14)])
         for i in range(15):
             self.board.append([])
             for j in range(15):
-                self.board[i].append
                 if (i,j) in triple_word:
-                    self.board[i].append(Cell(None, 1, 3))
-                print(self.board[i][j].word_mul, end=" ")
-
+                    self.board[i].append(Cell(None, 1, 3))                        
+                elif (i,j) in double_word:
+                    self.board[i].append(Cell(None, 1, 2))
+                elif (i,j) in triple_letter:
+                    self.board[i].append(Cell(None, 3, 1))
+                elif (i,j) in double_letter:
+                    self.board[i].append(Cell(None, 2, 1))
+                else:
+                    self.board[i].append(Cell(None, 1, 1))
+                print(self.board[i][j].letter_mul, end=" ")
+            print()
 
 class Cell:
     def __init__(self, letter, letter_mul, word_mul):
