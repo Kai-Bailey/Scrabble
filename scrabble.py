5eed2dd6e33558f3436ac189bd1b88090e1ad016
CodyGramlich <32050152+CodyGramlich@users.noreply.github.com>
2018-04-04 22:11:26 -0600

    Added exchange. Not fully working.

--- scrabble.py
+++ scrabble.py
@@ -12,7 +12,10 @@ class Player:
         # dictionary for placed tiles: key is rack index, value is (letter, (row, col))
         self.placed_tiles = {}
         self.selected_tile = ''
-        self.is_tile_selected = False
+        self.tile_selected = False
+
+        # dictionary for tiles to exchange: key is rack index, value is letter
+        self.tiles_to_exchange = {}
 
         self.init_rack()
 
@@ -97,6 +100,7 @@ class Game:
         self.current_player_number = 0
         self.current_player = self.players[self.current_player_number]
         self.running = True
+        self.exchanging = False
         self.tile_size = (self.surface.get_width()-200)/15
         self.text_color = (10, 10, 10)
 
@@ -150,7 +154,6 @@ class Game:
         self.draw_init()
         while self.running:
             self.handle_event()
-            self.update()
             self.draw_update()
 
     def draw_tile(self, i, j):
@@ -232,10 +235,6 @@ class Game:
         tile = pygame.Rect((0+self.tile_size*rack_tile, 0+self.tile_size*16, self.tile_size-1, self.tile_size-1))
         pygame.draw.rect(self.surface, (163, 81, 0), tile, 1)
 
-    def draw_unselected_rack_tile(self, rack_tile):
-        tile = pygame.Rect((0+self.tile_size*rack_tile, 0+self.tile_size*16, self.tile_size-1, self.tile_size-1))
-        pygame.draw.rect(self.surface, (188, 255, 243), tile, 1)
-
     def draw_remove_rack_tile(self, rack_tile):
         tile = pygame.Rect((0+self.tile_size*rack_tile, 0+self.tile_size*16, self.tile_size-1, self.tile_size-1))
         pygame.draw.rect(self.surface, (163, 81, 0), tile)
@@ -247,9 +246,13 @@ class Game:
         letter = font.render('Recall', 1, self.text_color)
         self.surface.blit(letter, (15+self.tile_size*1, 8+self.tile_size*15.25))
 
-    def draw_exchange(self):
+    def draw_exchange(self, exchanging):
         tile = pygame.Rect((0+self.tile_size*1, 0+self.tile_size*16.75, (self.tile_size-1)*2, self.tile_size-1))
-        pygame.draw.rect(self.surface, (247, 234, 0), tile)
+        if exchanging:
+            pygame.draw.rect(self.surface, (255, 30, 56), tile)
+        else:
+            pygame.draw.rect(self.surface, (247, 234, 0), tile)
+
         font = pygame.font.Font(None, 18)
         letter = font.render('Exchange', 1, self.text_color)
         self.surface.blit(letter, (5+self.tile_size*1, 8+self.tile_size*16.75))
@@ -279,7 +282,7 @@ class Game:
         font = pygame.font.Font(None, 22)
         for i in range(len(self.players)):
             letter = font.render(str(self.players[i].score), 1, self.text_color)
-            self.surface.blit(letter, (10+self.tile_size*18, 40+self.tile_size*i))
+            self.surface.blit(letter, (10+self.tile_size*20, 40+self.tile_size*i))
 
     def draw_current_player(self):
         font = pygame.font.Font(None, 22)
@@ -301,10 +304,14 @@ class Game:
         for i in range(4, 11):
             tile = pygame.Rect((0+self.tile_size*i, 0+self.tile_size*16, self.tile_size-1, self.tile_size-1))
             if tile.collidepoint(pos):
+
+                # draw deselect of previous selected
+                if self.current_player.tile_selected:
+                    self.draw_rack_tile(self.current_player.selected_tile+4)
                 # check that the rack tile is not empty
                 if self.current_player.rack[i-4] != '':
                     self.current_player.selected_tile = i - 4
-                    self.current_player.is_tile_selected = True
+                    self.current_player.tile_selected = True
                     self.draw_selected_rack_tile(i)
 
     def handle_place_tile(self, pos):
@@ -316,7 +323,7 @@ class Game:
                         # set letter on the board from rack
                         self.current_player.place_tile(self.current_player.selected_tile, i, j)
                         print(self.current_player.placed_tiles)
-                        self.current_player.is_tile_selected = False
+                        self.current_player.tile_selected = False
                         # draw remove tile from rack and tile on board
                         self.draw_remove_rack_tile(self.current_player.selected_tile+4)
                         self.draw_tile(i,j)
@@ -344,20 +351,45 @@ class Game:
                     self.draw_rack_tile(t+4)
 
                 # change to next player
-                self.handle_skip()
+                self.current_player.placed_tiles = {}
+                self.handle_endturn()
             else:
                 self.handle_recall()
-
-
-        self.current_player.placed_tiles = {}
+        print(self.current_player.rack)
 
     # changes to next player, redraws the rack and the scoreboard
-    def handle_skip(self):
+    def handle_endturn(self):
         self.current_player_number += 1
         self.current_player_number = self.current_player_number % len(self.players)
         self.current_player = self.players[self.current_player_number]
         self.draw_rack()
         self.draw_scoreboard()
+
+
+    # when in the exchanging state, handles select and deselect of tiles
+    def handle_exchange_select_tile(self, pos):
+        for i in range(4, 11):
+            tile = pygame.Rect((0+self.tile_size*i, 0+self.tile_size*16, self.tile_size-1, self.tile_size-1))
+            if tile.collidepoint(pos):
+                # if the tiles is already selected, deselect it by redrawing the rack tile
+                if (i-4) in self.current_player.tiles_to_exchange:
+                    self.current_player.tiles_to_exchange.pop(i-4)
+                    self.draw_rack_tile(i)
+
+                # check that the rack tile is not empty, then select it
+                elif self.current_player.rack[i-4] != '':
+                    self.current_player.tiles_to_exchange[i-4] = self.current_player.rack[i-4]
+                    self.draw_selected_rack_tile(i)
+
+    def handle_exchange(self):
+        if len(self.current_player.tiles_to_exchange) != 0:
+            for tile in self.current_player.tiles_to_exchange:
+                print(self.current_player.rack)
+                self.current_player.rack.remove(self.current_player.tiles_to_exchange[tile])
+                self.current_player.rack.insert(tile, self.board.draw_random_tile())
+            self.handle_endturn()
+        self.draw_rack()
+
 
 
     def handle_event(self):
@@ -370,17 +402,32 @@ class Game:
             play = pygame.Rect((0+self.tile_size*12, 0+self.tile_size*15.5, (self.tile_size-1)*2, self.tile_size-1))
             skip = pygame.Rect((0+self.tile_size*12, 0+self.tile_size*16.75, (self.tile_size-1)*2, self.tile_size-1))
             rack = pygame.Rect((0+self.tile_size*4, 0+self.tile_size*16, (self.tile_size-1)*7, self.tile_size-1))
-            if not self.current_player.is_tile_selected:
-                if rack.collidepoint(pos):
+            exchange = pygame.Rect((0+self.tile_size*1, 0+self.tile_size*16.75, (self.tile_size-1)*2, self.tile_size-1))
+
+            if rack.collidepoint(pos):
+                if self.exchanging:
+                    self.handle_exchange_select_tile(pos)
+                else:
                     self.handle_select_rack_tile(pos)
-                elif recall.collidepoint(pos):
-                    self.handle_recall()
-                elif play.collidepoint(pos):
-                    self.handle_play()
-                elif skip.collidepoint(pos):
-                    self.handle_recall()
-                    self.handle_skip()
-            else:
+            elif recall.collidepoint(pos):
+                self.handle_recall()
+            elif play.collidepoint(pos):
+                self.handle_play()
+            elif skip.collidepoint(pos):
+                self.handle_recall()
+                self.handle_endturn()
+            elif exchange.collidepoint(pos):
+                # recall any placed tiles
+                self.handle_recall()
+                if self.exchanging:
+                    self.draw_exchange(False)
+                    self.exchanging = False
+                    self.handle_exchange()
+                else:
+                    self.draw_exchange(True)
+                    self.draw_rack()
+                    self.exchanging = True
+            elif self.current_player.tile_selected:
                 self.handle_place_tile(pos)
 
     def draw_update(self):
@@ -392,16 +439,11 @@ class Game:
         self.draw_board()
         self.draw_rack()
         self.draw_recall()
-        self.draw_exchange()
+        self.draw_exchange(False)
         self.draw_play()
         self.draw_skip()
         self.draw_update()
         self.draw_scoreboard()
-
-
-
-    def update(self):
-        pass
 
 
 def main():
